ce3f4aa3d7a49b3e9aef7006546b13019c604f5c
Juan carlos IGLESIAS GONZALEZ <jiglesia@e3r9p4.42.fr>
2020-01-27 20:55:28 +0100

    a.

--- ft_printf.c
+++ ft_printf.c
@@ -6,24 +6,44 @@
 /*   By: jiglesia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 10:39:26 by jiglesia          #+#    #+#             */
-/*   Updated: 2020/01/20 15:21:55 by jiglesia         ###   ########.fr       */
+/*   Updated: 2020/01/27 20:54:19 by jiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int		ft_print(va_list ap, char *flags)
+int		ft_checkflags(int *i, char *flags, va_list ap, int sum)
 {
+	while (flags[i] && (flags[i] != '%' || flags[i] != ' '))
+	{
+		
+	}
+	return (sum);
+}
 
+int		ft_print(va_list ap, char *flags, int sum)
+{
+	int i;
+
+	i = 0;
+	while (flags[i])
+	{
+		if (flags[i] == '%')
+			sum = ft_checkflags(&i, flags, ap, sum);
+		else
+		{
+			ft_putchar(flags[i++]);
+			sum++;
+		}
+	}
+	return (sum);
 }
 
 int		ft_printf(const *flags, ...)
 {
 	va_list		ap;
-	int			numchar;
+	int			sum;
 
 	va_start(ap, flags);
-	numchar = ft_print(ap, flags);
+	sum = ft_print(ap, flags, sum);
 	va_end(ap);
-	return (numchar);
+	return (sum);
 }
